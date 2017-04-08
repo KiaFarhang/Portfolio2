a44e1b71ebf13252ba6a447f181ff422c9491a04
Kia Farhang <kfarhang0@gmail.com>
2017-04-08 13:34:25 -0700

    Test

--- webpack.js
+++ webpack.js
@@ -9,9 +9,21 @@ let options = {
 };
 
 app.use(express.static('dist'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "https://www.solarsmartbuy.com");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.get('/', function(req, res){
 	res.sendFile('dist/index/html', options);
+});
+
+app.get('/example1', function(req, res){
+	res.status(200).send('Hello there!');
 });
 
 app.listen(8082);
