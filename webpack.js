9b43e89f4764185e9e9b2bfef89cbc95d45a711f
Kia Farhang <kfarhang0@gmail.com>
2017-04-08 18:04:42 -0700

    More endpoints

--- webpack.js
+++ webpack.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 var app = express();
 
 let options = {
-	root: __dirname
+    root: __dirname
 };
 
 app.use(express.static('dist'));
@@ -14,17 +14,31 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
 });
 
-app.get('/', function(req, res){
-	res.sendFile('dist/index/html', options);
+app.get('/', function(req, res) {
+    res.sendFile('dist/index/html', options);
 });
 
-app.get('/example1', function(req, res){
-	res.status(200).send('Hello there!');
+app.get('/example1', function(req, res) {
+    res.status(200).send('Hello there!');
+});
+
+app.get('/example2', function(req.res) {
+    let obj = {
+        make: 'Honda',
+        model: 'Civic',
+        year: 1999
+    };
+
+    res.status(200).json(obj);
+});
+
+app.get('/example3', function(req, res){
+	res.status.sendFile('dogs.json');
 });
 
 app.listen(8082);
